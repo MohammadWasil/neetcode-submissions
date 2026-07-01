f54ff13c24578f2d25bfbe708b4fe0629248d58f
Mohammad Wasil Saleem <31696557+MohammadWasil@users.noreply.github.com>
2026-07-01 22:57:32 +0200

    Add: top-k-elements-in-list - submission-3

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-3.py
@@ -0,0 +1,26 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        """
+        [1, 2, 2, 3, 3, 3] ->k = 2, res = [2, 3]
+        hashmap = {1: 1, 2:1+1, 3:1+1+1} -> select the k key with the maximum value
+        hashmap = {1:1, 2:2, 3:3}
+        """
+
+        # Create hashmaps
+        hashmap = defaultdict(int)
+
+        # populate them
+        for i in nums:
+            hashmap[i] += 1
+        
+        # better to convertthem into list of tuples
+        hashlist = [(k, v) for k, v in hashmap.items()]
+
+        # sort them based on the second value of the tuple
+        sorted_list = sorted(hashlist, key=lambda element: element[1], reverse=True)
+        
+        return [pair[0] for pair in sorted_list[:k]]
+
+
+
+        
