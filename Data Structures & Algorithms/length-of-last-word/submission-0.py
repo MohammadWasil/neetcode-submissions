61416cab44811f5e7e225fd27a31f1789d158172
Mohammad Wasil Saleem <31696557+MohammadWasil@users.noreply.github.com>
2026-09-21 23:15:03 +0200

    Add: length-of-last-word - submission-0

---
+++ Data Structures & Algorithms/length-of-last-word/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        """
+        s = "Hello World"
+        Strip all the empty white spaces on the right
+        split the string based on the empty space, and put it into list
+        take the last elemene tomfr the list
+        count the len of the last element
+        """
+
+        return len(s.rstrip().split(" ")[-1])
